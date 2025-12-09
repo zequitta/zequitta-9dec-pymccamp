8c41a18e0b4b4f747d76ec51639f2e4407790e38
zequitta <zequitta367@gmail.com>
2025-12-09 14:16:41 +0800

    Initial commit

---
+++ Day1Folder/minecraft_day1.py
@@ -0,0 +1,6 @@
+# AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
+# THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+################# Function Section #############################
+
+
+################## On Chat Commands Section #####################
---
+++ Day4Folder/day4.py
@@ -0,0 +1,16 @@
+# Write all your codes for Day 4 here.
+# COMMENT out the previous task before going on to the next task
+print("hello from day4")
+
+########################################################################
+# Task 1:
+
+
+
+########################################################################
+# Task 2:
+
+
+
+########################################################################
+# Additional exercises:
